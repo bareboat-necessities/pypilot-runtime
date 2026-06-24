af1f5eea56c54bd7c160aa17113950ef0b2b3d08
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-24 16:33:44 -0400

    Update server compile test for data model runtime

--- tests/test_runtime_server_compile.cpp
+++ tests/test_runtime_server_compile.cpp
@@ -4,17 +4,8 @@
 
 int main() {
     pypilot_event_loop::EventLoop<8, 128> loop;
-
-    pypilot_runtime::AutopilotValues autopilot;
-    pypilot_runtime::BoatImuValues boatimu;
-    pypilot_runtime::ServoValues servo;
-    pypilot_runtime::SensorValues sensors;
-    pypilot_runtime::PilotValues pilots;
-    pypilot_runtime::GpsValues gps;
-    pypilot_runtime::WindValues wind;
-
-    pypilot_runtime::PypilotRuntimeState state{autopilot, boatimu, sensors, servo, pilots, gps, wind};
-    pypilot_runtime::PypilotRuntimeProtocol protocol(state);
+    pypilot_runtime::PypilotRuntimeState model;
+    pypilot_runtime::PypilotRuntimeProtocol protocol(model);
     pypilot_runtime::PypilotRuntimeServer<2, 4> server(loop, protocol);
 
     pypilot_runtime::PypilotRuntimeServerOptions options;
@@ -31,7 +22,6 @@ int main() {
 
     pypilot_event_loop::TcpWatermarkOptions watermarks;
     watermarks.read_low = 1;
-    watermarks.write_low = 0;
     server.set_tcp_watermarks(watermarks);
     server.set_max_output_bytes(4096);
 
