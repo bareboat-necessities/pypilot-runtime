f8acab47565bb44c97f3610b867441b82661c5a2
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-26 12:44:41 -0400

    Align runtime settings bridge test with writable values

--- tests/test_runtime_settings_bridge.cpp
+++ tests/test_runtime_settings_bridge.cpp
@@ -16,10 +16,8 @@ int main() {
     char error[160]{};
     assert(settings.save_value("ap.mode", "wind", error, sizeof(error)));
     assert(settings.save_value("ap.pilot", "basic", error, sizeof(error)));
-    assert(settings.save_value("servo.engaged", "true", error, sizeof(error)));
     assert(pypilot_runtime::RuntimeSettingsBridge::load(protocol, settings));
     assert(model.ap.mode.value == ship_data_model::AutopilotMode::wind);
-    assert(model.servo.engaged.value);
 
     assert(pypilot_runtime::RuntimeSettingsBridge::apply_set_and_save(protocol, settings, "ap.mode", "gps", error, sizeof(error)));
     assert(model.ap.mode.value == ship_data_model::AutopilotMode::gps);
